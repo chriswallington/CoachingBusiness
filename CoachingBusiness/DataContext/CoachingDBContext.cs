41422d97f779dc8e6b44dd2b4b3b01290da5701e
Chris Wallington <chris@carpedatum.co.uk>
2020-02-21 11:42:02 +0000

    Add project files.

---
+++ CoachingBusiness/DataContext/CoachingDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoachingBusiness.DataContext
+{
+    public class CoachingDBContext : DbContext
+    {
+        public CoachingDBContext(DbContextOptions<CoachingDBContext> options) : base(options)
+        {
+        }
+        
+
+    }
+}
