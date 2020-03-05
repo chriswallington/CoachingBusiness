b443c8aa031f61f99c7ee8f837bd8934330f0fd9
Chris Wallington <chris@carpedatum.co.uk>
2020-03-05 15:52:46 +0000

    Add DataSeeding to CoachingDBContext

--- CoachingBusiness/DataContext/CoachingDBContext.cs
+++ CoachingBusiness/DataContext/CoachingDBContext.cs
@@ -1,5 +1,6 @@
 ﻿using CoachingBusiness.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,50 @@ namespace CoachingBusiness.DataContext
         {
         }
         public DbSet<UserModel> Users { get; set; }
-        public DbSet<TaskModel> Tasks { get; set; }        
+        public DbSet<TaskModel> Tasks { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserModel>().HasData(
+                new UserModel
+                {
+                    Id = 1,
+                    Firstname = "Chris",
+                    Surname = "Wallington",
+                    Role = "Coach"
+                },
+                new UserModel
+                {
+                    Id = 2,
+                    Firstname = "Laurence",
+                    Surname = "Webb",
+                    Role = "Client"
+                }
+                );
+            modelBuilder.Entity<TaskModel>().HasData(
 
+                new TaskModel
+                { 
+                    Id = 1,
+                    UserId = 2,
+                    TaskName = "Increase Prices",
+                    Details = "Bulk price increase by 2%",
+                    Status = "In Progress",
+                    Archived = false
+                },
+                new TaskModel
+                {
+                    Id = 2,
+                    UserId = 2,
+                    TaskName = "Reduce workload",
+                    Details = "Identify jobs that other members of staff could take on.",
+                    Status = "Not Started",
+                    Archived = false
+                }
+                );
+
+            modelBuilder.Entity<TaskModel>().
+        }
     }
+
+
 }
